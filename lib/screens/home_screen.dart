9b97f437fc04c2d7bef32eaa0f682bf80294b824
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-11-10 04:42:59 -0300

    Added Post Feed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -56,15 +56,15 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
           ),
-          // SliverList(
-          //   delegate: SliverChildBuilderDelegate(
-          //         (context, index) {
-          //       final Post post = posts[index];
-          //       return PostContainer(post: post);
-          //     },
-          //     childCount: posts.length,
-          //   ),
-          // ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                final Post post = posts[index];
+                return PostContainer(post: post);
+              },
+              childCount: posts.length,
+            ),
+          ),
         ],
       ),
     );
