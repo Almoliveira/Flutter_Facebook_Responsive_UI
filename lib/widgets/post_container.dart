1679ff1779119d3cc055d6294e3d841da0ca9ecc
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-11-12 05:35:59 -0300

    Added Contacts and Options List

--- lib/widgets/post_container.dart
+++ lib/widgets/post_container.dart
@@ -1,5 +1,4 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_responsive_ui/config/palette.dart';
 import 'package:flutter_facebook_responsive_ui/models/models.dart';
@@ -16,36 +15,47 @@ class PostContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 5.0),
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      color: Colors.white,
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                _PostHeader(post: post),
-                const SizedBox(height: 4.0),
-                Text(post.caption),
-                post.imageUrl != null
-                    ? const SizedBox.shrink()
-                    : const SizedBox(height: 6.0),
-              ],
-            ),
-          ),
-          post.imageUrl != null
-              ? Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: CachedNetworkImage(imageUrl: post.imageUrl),
-                )
-              : const SizedBox.shrink(),
-          Padding(
+    final bool isDesktop = Responsive.isDesktop(context);
+    return Card(
+      margin: EdgeInsets.symmetric(
+        vertical: 5.0,
+        horizontal: isDesktop ? 5.0 : 0.0,
+      ),
+      elevation: isDesktop ? 1.0 : 0.0,
+      shape: isDesktop
+          ? RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0))
+          : null,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        color: Colors.white,
+        child: Column(
+          children: [
+            Padding(
               padding: const EdgeInsets.symmetric(horizontal: 12.0),
-              child: _PostStats(post: post))
-        ],
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  _PostHeader(post: post),
+                  const SizedBox(height: 4.0),
+                  Text(post.caption),
+                  post.imageUrl != null
+                      ? const SizedBox.shrink()
+                      : const SizedBox(height: 6.0),
+                ],
+              ),
+            ),
+            post.imageUrl != null
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: CachedNetworkImage(imageUrl: post.imageUrl),
+                  )
+                : const SizedBox.shrink(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: _PostStats(post: post),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -129,9 +139,7 @@ class _PostStats extends StatelessWidget {
                 color: Colors.white,
               ),
             ),
-            const SizedBox(
-              width: 4.0,
-            ),
+            const SizedBox(width: 4.0),
             Expanded(
               child: Text(
                 '${post.likes}',
@@ -152,7 +160,7 @@ class _PostStats extends StatelessWidget {
               style: TextStyle(
                 color: Colors.grey[600],
               ),
-            ),
+            )
           ],
         ),
         const Divider(),
@@ -184,9 +192,9 @@ class _PostStats extends StatelessWidget {
               ),
               label: 'Share',
               onTap: () => print('Share'),
-            ),
+            )
           ],
-        )
+        ),
       ],
     );
   }
--- lib/widgets/responsive.dart
+++ lib/widgets/responsive.dart
@@ -7,9 +7,9 @@ class Responsive extends StatelessWidget {
 
   const Responsive({
     Key key,
-    this.mobile,
+    @required this.mobile,
     this.tablet,
-    this.desktop,
+    @required this.desktop,
   }) : super(key: key);
 
   static bool isMobile(BuildContext context) =>
@@ -24,14 +24,16 @@ class Responsive extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constraints) {
-      if (constraints.maxWidth >= 1200) {
-        return desktop;
-      } else if (constraints.maxWidth >= 800) {
-        return tablet ?? mobile;
-      } else {
-        return mobile;
-      }
-    });
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth >= 1200) {
+          return desktop;
+        } else if (constraints.maxWidth >= 800) {
+          return tablet ?? mobile;
+        } else {
+          return mobile;
+        }
+      },
+    );
   }
 }
